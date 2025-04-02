c71fb21c93ad2dd7ee384e6a1331277cfefdfea9
Konrad Głąb <Darnok10000@gmail.com>
2025-04-02 22:32:42 +0200

    add orderManagement

---
+++ VirtualThreadsTestApp/src/main/java/pl/edu/ug/kglab/VirtualThreadsTestApp/orderManagement/ProductInfo.java
@@ -0,0 +1,11 @@
+package pl.edu.ug.kglab.VirtualThreadsTestApp.orderManagement;
+
+import lombok.Data;
+
+@Data
+public class ProductInfo {
+
+    private String name;
+
+    private String description;
+}
