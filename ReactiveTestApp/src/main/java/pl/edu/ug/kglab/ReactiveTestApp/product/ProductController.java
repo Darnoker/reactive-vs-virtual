62b369002f9fa8e6bb54262adfeb513d0934ff41
Konrad Głąb <73139334+Darnoker@users.noreply.github.com>
2025-04-03 19:09:25 +0200

    fixes (#5)

--- ReactiveTestApp/src/main/java/pl/edu/ug/kglab/ReactiveTestApp/product/ProductController.java
+++ ReactiveTestApp/src/main/java/pl/edu/ug/kglab/ReactiveTestApp/product/ProductController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.edu.ug.kglab.ReactiveTestApp.common.RollbackException;
 import pl.edu.ug.kglab.ReactiveTestApp.product.dto.CreateReviewRequestBody;
 import pl.edu.ug.kglab.ReactiveTestApp.product.model.Review;
 import reactor.core.publisher.Mono;
--- VirtualThreadsTestApp/src/main/java/pl/edu/ug/kglab/VirtualThreadsTestApp/user/UserController.java
+++ VirtualThreadsTestApp/src/main/java/pl/edu/ug/kglab/VirtualThreadsTestApp/user/UserController.java
@@ -2,6 +2,7 @@ package pl.edu.ug.kglab.VirtualThreadsTestApp.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.edu.ug.kglab.VirtualThreadsTestApp.user.model.User;
 
@@ -9,6 +10,7 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -18,7 +20,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("")
+    @GetMapping("/lastname/{lastname}")
     public CompletableFuture<List<User>> findUsersByLastname(@PathVariable String lastname) {
         return CompletableFuture.supplyAsync(() -> userService.findUsersByLastname(lastname));
     }
