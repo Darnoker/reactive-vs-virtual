d6896ac1aa5d460fdcfb7760e202e88daa299390
Konrad Głąb <Darnok10000@gmail.com>
2025-04-18 22:38:50 +0200

    update health controller

--- src/main/java/pl/edu/ug/kglab/VirtualThreadsTestApp/health/HealthController.java
+++ src/main/java/pl/edu/ug/kglab/VirtualThreadsTestApp/health/HealthController.java
@@ -1,6 +1,7 @@
 package pl.edu.ug.kglab.VirtualThreadsTestApp.health;
 
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,12 @@ import java.util.concurrent.CompletableFuture;
 @RestController
 public class HealthController {
 
-    @GetMapping("/thread-health")
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @GetMapping("/myHealth")
     @SneakyThrows
     public CompletableFuture<String> getThreadName() {
-        return CompletableFuture.supplyAsync(() -> Thread.currentThread().toString());
+        return CompletableFuture.supplyAsync(() -> Thread.currentThread() + " " + appName);
     }
 }
