1def465ea627edfefa57f4f01540e286c6b09f77
Konrad Głąb <Darnok10000@gmail.com>
2025-04-18 11:36:33 +0200

    return appName

--- src/main/java/pl/edu/ug/kglab/ReactiveTestApp/health/HealthController.java
+++ src/main/java/pl/edu/ug/kglab/ReactiveTestApp/health/HealthController.java
@@ -1,13 +1,17 @@
 package pl.edu.ug.kglab.ReactiveTestApp.health;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
+    @Value("${spring.application.name}")
+    private String appName;
+
     @GetMapping("/myHealth")
     public String get() {
-        return "OK";
+        return appName;
     }
 }
